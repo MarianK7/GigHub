eadd508a8b9e1cab99ddaad4fe6e9b2aab09fe9b
Marián Keszi <marian.keszi@gmail.com>
2023-07-04 16:46:24 +0200

    Added some basic data to show at the home page

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,27 @@
+<h1>{{$heading}}</h1>
+<p>Here are all current listings.</p>
+
+@unless (count($listings))
+    
+@endunless
+@foreach ($listings as $listing)
+    <h2>
+        {{$listing['title']}}
+    </h2>
+    <p>
+        {{$listing['description']}}
+    </p>
+    <p>
+        <strong>
+            Budget: 
+        </strong>
+
+        {{$listing['budget']}}
+    </p>
+@endforeach
+
+@else
+    <p>
+        There are no listings.
+    </p>
+@endunless
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest gigs',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Need a WordPress site built',
+                'description' => 'I need a WordPress site built for my business. I already have a design, I just need you to build the theme.',
+                'budget' => 5000
+            ],
+            [
+                'id' => 2,
+                'title' => 'Laravel developer needed',
+                'description' => 'I need someone to complete my Laravel website. I already have a design, I just need you to build a few pages.',
+                'budget' => 3000
+            ]
+            
+        ]
+    ]);
 });
