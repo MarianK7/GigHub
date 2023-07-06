8079f7382540939468d704f9d08b28767f5cd2e2
Marián Keszi <marian.keszi@gmail.com>
2023-07-06 14:32:16 +0200

    Listing data are now stored in DB
    added migration and factory for listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listing::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        /* Listing::create([
+            'title' => 'Laravel senior developer',
+            'tags' => 'laravel,php,backend',
+            'company' => 'Acme Corp',
+            'location' => 'Remote',
+            'email' => 'email@gmail.com',
+            'website' => 'https://acme.com',
+            'description' => 'This is a test job description.'
+        ]);
+
+        Listing::create([
+            'title' => 'Laravel junior developer',
+            'tags' => 'laravel,php,backend',
+            'company' => 'Acme Corp',
+            'location' => 'Remote',
+            'email' => 'email2@gmail.com',
+            'website' => 'https://acme.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, quis aliquam'
+        ]); */
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,23 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All listings
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest gigs',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Need a WordPress site built',
-                'description' => 'I need a WordPress site built for my business. I already have a design, I just need you to build the theme.',
-                'budget' => 5000
-            ],
-            [
-                'id' => 2,
-                'title' => 'Laravel developer needed',
-                'description' => 'I need someone to complete my Laravel website. I already have a design, I just need you to build a few pages.',
-                'budget' => 3000
-            ]
-            
-        ]
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
